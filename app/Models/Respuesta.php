880ac8f00a8dc5b08b5f11d75de9fa7070f5d69f
Josh2298 <edward98jm@gmail.com>
2021-06-13 15:24:28 -0400

    se modifico

--- app/Models/Respuesta.php
+++ app/Models/Respuesta.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Respuesta extends Model
 {
     use HasFactory;
-    use SoftDeletes;
     protected $table='respuestas';
     protected $fillable=['iddato','CO_1_P1',
     'CO_1_P2',
